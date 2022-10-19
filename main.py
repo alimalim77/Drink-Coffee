6940f95135a6287d13d6629a4a2fb98516ba6775
alimalim77 <52186295+alimalim77@users.noreply.github.com>
2022-10-20 05:04:24 +0530

    Notifier

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PIL import Image, ImageTk
 root = Tk()
 root.geometry("550x550")
 root.title("Drink Water Notification")
-#root.resizable(False,False)
+root.resizable(False,False)
 
 
 
@@ -14,12 +14,19 @@ main = LabelFrame(root,padx= 100, pady = 7, borderwidth=3, relief="raised")
 main.grid(row=0,column=1,padx=20,pady=60)
 
 text_label = Label(main,text="Drinking Water reminder", font=("comic Sans ms",14,"bold"),bg="#C4C4C4")
-text_label.grid(row=0,column=20,padx=20,pady=10)
+text_label.grid(row=0,column=0,padx=20,pady=10)
 
-image = ImageTk.PhotoImage(Image.open("dc.jpg"))
+image = Image.open("dc.jpg")
+img = image.resize((150,150))
+my_img = ImageTk.PhotoImage(img)
 
-img_label = Label(main,image=image)
+
+
+
+img_label = Label(main,image=my_img)
 img_label.grid(row = 1,column = 0, pady=10,padx=20)
 
+inp = Entry(main,width=30)
+inp.grid(row=0,col=2,padx=30)
 
 root.mainloop()
